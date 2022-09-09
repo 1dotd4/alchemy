23f0a86acef20c546d9cbc5e1ab77399dd6d167f
d4 <d4@unpx.net>
2022-09-09 19:04:28 +0200

    Add and verify square-linear-system

--- alchemy.linear-algebra.scm
+++ alchemy.linear-algebra.scm
@@ -1,7 +1,10 @@
 (define-library (alchemy linear-algebra)
   (export 
+    range
     ma-swap-col!
     ma-pp
+    v-pp
+    square-linear-system
     rho
     transpose)
   (import (scheme base)
@@ -18,15 +21,46 @@
           '()
           (fold append '() (map (lambda (x) (map (lambda (y) (list x y)) ys)) xs))))
 
+    ;; [i , j)
+    ;; 3 4 5 6
+    ;; 0 1 2 3
+    ;; => 4
+    (define (range i j)
+      (iota (- j i) i 1))
+
+    (define (v-swap! vec i j)
+      (let ((t (vector-ref vec i)))
+        (vector-set! vec i (vector-ref vec j))
+        (vector-set! vec j t)))
+
+    (define (v-pp vec)
+      (let ((len (vector-length vec)))
+        (display "[ ")
+        (map (lambda (i)
+               (begin
+                 (display (number->string (vector-ref vec i))))
+               (if (= len (+ 1 i))
+                 (display " ]\n")
+                 (display ", ")))
+             (iota len))))
+
     ;; Note that matrix is represented column-wise
     (define (ma matrix row column) (vector-ref (vector-ref matrix column) row))
-    (define (ma-set! matrix row column value) (vector-set! (vector-ref matrix column) row value))
+    (define (ma-set! matrix row column value)
+      (vector-set! (vector-ref matrix column) row value)
+      matrix)
 
     (define (ma-swap-col! matrix i j)
       (let ((tmp (vector-copy (vector-ref matrix i))))
         (vector-set! matrix i (vector-ref matrix j))
         (vector-set! matrix j tmp)
         matrix))
+
+    (define (ma-swap! A ai aj bi bj)
+      (let ((tmp (ma A ai aj)))
+        (ma-set! A ai aj (ma A bi bj))
+        (ma-set! A bi bj tmp)
+        A))
 
     (define (ma-pp A)
       (let ((columns (vector-length A))
@@ -64,6 +98,136 @@
           (lambda (x) (ma-set! B (car x) (cadr x) (ma A (cadr x) (car x))))
           (cartesian-product (iota new-rows) (iota new-columns)))
         B))
+
+    (define (vector-swap! v i j)
+      (let ((tmp (vector-ref v i)))
+        (vector-set! v i (vector-ref v j))
+        (vector-set! v j tmp)
+        v))
+
+;     ;; 2.2.1
+;     (define (square-linear-system M B)
+;       (let ((n (vector-length A))
+;             (C (make-vector n)))
+;       ;; M is nxn
+; 
+;       (define (find-non-zero-i A j)
+;         (let rec ((i j))
+;           (cond
+;             ((= n i) #f)
+;             ((zero? (ma A i j)) (rec (+ 1 i)))
+;             (else i))))
+;       (define (swap-step! A i j)
+;         (map
+;           (lambda (l)
+;             (ma-swap! A i l j l))
+;           (range j n)))
+;       (define (divide-row! A i j)
+;         (let ((C (make-vector n)))
+;           (map
+;             (lambda (l)
+;               )
+;             (range j n))))
+; 
+;       (let rec ((j 0))
+;         (if (> j n)
+;           (solve-triangular M B)
+;           (let ((i (find-non-zero-i M j)))
+;             (if (not i)
+;               #f
+;               (begin
+;                 ;; swap if needed
+;                 (if (> i j)
+;                   (begin
+;                     (swap-step! M i j)
+;                     (vector-swap! B i j)))
+;                 ;; eliminate
+;                 (let ((d (/ 1 (ma M j j))))
+;                   (map ... fill my C )
+;                   (map
+;                     (lambda (k)
+;                       (map
+;                         (lambda (l)
+; 
+;                           )
+;                         (range ?? n)) ;; columns
+;                       (vector-set! B k ??))
+;                     (range ?? n))) ; rows
+
+
+    ;; 2.2.1
+    (define (square-linear-system M-orig B-orig)
+      (define n (vector-length M-orig))
+      (define M (vector-copy M-orig))
+      (define B (vector-copy B-orig))
+      (define C (make-vector n 0))
+      (define X (make-vector n 0))
+      (let loop ((j 0) (i 0))
+        (cond
+          ; solve linear system
+          [(= j n)
+           ; Here M is an upper triangular matrix
+           (do ((i (- n 1) (- i 1)))
+             ((< i 0) X)
+             (vector-set! X i
+                          (/ (- (vector-ref B i)
+                                (fold
+                                  (lambda (j s) (+ s (* (ma M i j) (vector-ref X j))))
+                                  0
+                                  (range i n)))
+                             (ma M i i))))]
+          ; 3. all zero entry
+          [(= n i)
+           (begin
+             (ma-pp M)
+             (error "Not invertible matrix!"))]
+          ; 3. find non-zero entry
+          [(zero? (ma M i j)) (loop j (+ 1 i))]
+          ; coninue
+          (else
+            (begin
+              ; 4. Swap?
+              (if (> i j)
+                (begin
+                  (display "I need to swap!")
+                  (do ((l j (+ l 1)))
+                    ((= l n) '())
+                      (ma-swap! M i l j l))
+                  (v-swap! B i j)))
+              ; 5. elminiate from M[j,j]
+              ; Get the pivot inverse and apply to current columnt that will be propagated
+              (let ((d (/ 1 (ma M j j))))
+                (do ((k (+ j 1) (+ k 1)))
+                  ((= k n) '())
+                  (vector-set! C k (* d (ma M k j)))))
+              ; >"Note that we do no need to compute this"
+              ; Yeah but if you don't say I have to set it...
+              (do ((k (+ j 1) (+ k 1)))
+                ((= k n) '())
+                (ma-set! M k j 0))
+              ; Loop to eliminate on matrix
+              (do ((k (+ j 1) (+ k 1)))
+                ((= k n) '())
+                (do ((l (+ j 1) (+ l 1)))
+                  ((= l n) '())
+                  (ma-set! M k l (- (ma M k l)
+                                    (* (vector-ref C k)
+                                       (ma M j l))))))
+              ; Loop to propagate on the constants
+              (do ((k (+ j 1) (+ k 1)))
+                ((= k n) '())
+                (vector-set! B k
+                             (- (vector-ref B k)
+                                (* (vector-ref C k)
+                                   (vector-ref B j)))))
+              ; continue with the next column
+              (loop (+ j 1) (+ j 1)))))))
+
+
+
+
+
+
 
 
     ))
--- alchemy.linear-algebra.test.scm
+++ alchemy.linear-algebra.test.scm
@@ -3,13 +3,24 @@
         (alchemy linear-algebra))
 
 
-(ma-pp (rho 3 4 '(1 2 3 4
-                  5 6 7 8
-                  9 10 11 12)))
+(ma-pp (rho 3 4 '(1 2 3 4 5 6 7 8 9 10 11 12)))
 
-(ma-pp (transpose (rho 3 4 '(1 2 3 4
-                  5 6 7 8
-                  9 10 11 12))))
+(ma-pp (transpose (rho 3 4 '(1 2 3 4 5 6 7 8 9 10 11 12))))
 
 (ma-pp (ma-swap-col! (rho 3 4 '(1 2 3 4 5 6 7 8 9 10 11 12))
                      1 2))
+
+(ma-pp (rho 2 2 '(2 3 5 6)))
+(v-pp
+  (square-linear-system
+    (rho 2 2 '(2 3 5 6))
+    (vector 4 7)))
+
+(v-pp
+  (square-linear-system
+    (rho 4 4 '( 1  1 -3  1
+               -5  3 -4  1
+                1  0  2 -1
+                1  2  0  0))
+    (vector 2 0 1 12)))
+
